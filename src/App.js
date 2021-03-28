e6e6fc9009e96d3a004a4cd214a2a0a25aff8f52
Raylson Alves <raaylsonalves@gmail.com>
2021-03-27 21:05:57 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import PokedexComponent from './components/Pokedex/PokedexComponent'
+import 'antd/dist/antd.css'
+
+function App() {
+  return (
+    <div className="App">
+      <PokedexComponent />
+    </div>
+  );
+}
+
+export default App;
